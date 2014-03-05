9874c93155fc3bb6982e8d68fddab5b1582de497
Nathan Matteson <matteson@obstructures.org>
2014-03-05 08:31:28 -0600

    Add template filters

--- atlas/__init__.py
+++ atlas/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
-
+from atlas.filters import safe_markdown, format_currency, smartypants, nbsp
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+app.jinja_env.filters['markdown'] = safe_markdown
+app.jinja_env.filters['format_currency'] = format_currency
+app.jinja_env.filters['smartypants'] = smartypants
+app.jinja_env.filters['nbsp'] = nbsp
 
 from atlas.views import mod as atlas_mod
 app.register_blueprint(atlas_mod)
