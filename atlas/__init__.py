84b38e2b83f31c18f9bb21a01ca78d615cf98909
Nathan Matteson <matteson@obstructures.org>
2015-07-27 08:26:29 -0500

    Move flask app to atlas_web

--- atlas/__init__.py
+++ atlas/__init__.py
@@ -1,14 +1,1 @@
-from flask import Flask
-from atlas.filters import safe_markdown, format_currency, smartypants, nbsp, \
-    nowrap
-
-app = Flask(__name__)
-app.config.from_object('config')
-app.jinja_env.filters['markdown'] = safe_markdown
-app.jinja_env.filters['format_currency'] = format_currency
-app.jinja_env.filters['smartypants'] = smartypants
-app.jinja_env.filters['nbsp'] = nbsp
-app.jinja_env.filters['nowrap'] = nowrap
-
-from atlas.views import mod as atlas_mod
-app.register_blueprint(atlas_mod)
+# -*- coding: utf-8 -*-
--- atlas_web/__init__.py
+++ atlas_web/__init__.py
@@ -1,2 +1,15 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import Flask
+from atlas_web.filters import safe_markdown, format_currency, smartypants, \
+    nbsp, nowrap
+
+app = Flask(__name__)
+app.config.from_object('config')
+app.jinja_env.filters['markdown'] = safe_markdown
+app.jinja_env.filters['format_currency'] = format_currency
+app.jinja_env.filters['smartypants'] = smartypants
+app.jinja_env.filters['nbsp'] = nbsp
+app.jinja_env.filters['nowrap'] = nowrap
+
+from atlas_web.views import mod as atlas_mod
+app.register_blueprint(atlas_mod)
