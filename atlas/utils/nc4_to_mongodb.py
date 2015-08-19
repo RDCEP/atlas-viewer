b1c0f5c1e61c968a1137110db77e766bd8a415e8
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2015-08-19 09:17:29 -0500

    Inclusion of pymongo library and general code fixing

--- atlas/utils/nc4_to_mongodb.py
+++ atlas/utils/nc4_to_mongodb.py
@@ -13,6 +13,11 @@ try:
 except ImportError:
     from osgeo import osr
 
+try:
+    import pymongo
+except ImportError:
+    print 'MongoDB API not available.'
+
 # input location of the netCDF file
 nc_file = "/home/ricardo/estagio/papsim_wfdei.cru_hist_default_firr_aet_whe_annual_1979_2012.nc4"
 
@@ -25,30 +30,35 @@ longitude = 'lon'
 time = 'time'
 value = 'aet_whe'
 
-# get number of time (time dimension)
-num_time = len(nc_dataset.dimensions[time])
-
 # get netCDF variable objects
 latitudes = nc_dataset.variables[latitude]
 longitudes = nc_dataset.variables[longitude]
 values = nc_dataset.variables[value]
+times = nc_dataset.dimensions[time]
+
+# get array sizes
+num_lat = len(latitudes)
+num_lon = len(longitudes)
+num_time = len(times)
 
 # get netCDF variable values
 lats = latitudes[:]
 lons = longitudes[:]
 vals = values[:, :, :, :]
+tims = times[:]
 
 # make a list of latitudes and longitudes
 latitudes = [int(i) for i in lats]
 longitudes = [int(i) for i in lons]
+times = [int(i) for i in tims]
 
 # populating the database
 try:
-    for lat in range(len(latitudes)):
+    for lat in range(num_lat):
         try:
-            for lon in range(len(longitudes)):
+            for lon in range(num_lon):
                 try:
-                    for time in range(num_time):
+                    for tyme in range(num_time):
                 # TODO: Needs to write commit on mongodb collection (still to define)
                 except Exception:
                     print 'Error while reading time'
