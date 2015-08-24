5fa819d2c467e4beced8dacd8871fa2626549d6d
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2015-08-24 11:12:14 -0500

    Major fix on nc4 indexing, and georeferencing

--- atlas/utils/nc4_to_mongodb.py
+++ atlas/utils/nc4_to_mongodb.py
@@ -1,7 +1,11 @@
 __author__ = 'ricardo'
 # 2015-08-19 - Initial commit
+# coding: utf-8
 
 from netCDF4 import Dataset
+
+# Timestamp for testing
+import datetime
 
 try:
     import ogr
@@ -25,8 +29,8 @@ nc_file = "/home/ricardo/estagio/papsim_wfdei.cru_hist_default_firr_aet_whe_annu
 nc_dataset = Dataset(nc_file, 'r')
 
 # netCDF variables
-latitude = 'lat'
 longitude = 'lon'
+latitude = 'lat'
 time = 'time'
 value = 'aet_whe'
 
@@ -34,36 +38,38 @@ value = 'aet_whe'
 latitudes = nc_dataset.variables[latitude]
 longitudes = nc_dataset.variables[longitude]
 values = nc_dataset.variables[value]
-times = nc_dataset.dimensions[time]
-
-# get array sizes
-num_lat = len(latitudes)
-num_lon = len(longitudes)
-num_time = len(times)
+times = nc_dataset.variables[time]
 
 # get netCDF variable values
 lats = latitudes[:]
 lons = longitudes[:]
-vals = values[:, :, :, :]
+vals = values[:, :, :]  # Sequence: Time, Latitude, Long
 tims = times[:]
 
 # make a list of latitudes and longitudes
-latitudes = [int(i) for i in lats]
-longitudes = [int(i) for i in lons]
-times = [int(i) for i in tims]
+count_lat = [int(i) for i in lats]
+count_long = [int(i) for i in lons]
+count_time = [int(i) for i in tims]
 
 # populating the database
 try:
-    for lat in range(num_lat):
+    for lat in range(len(count_lat)):
         try:
-            for lon in range(num_lon):
+            for lon in range(len(count_long)):
                 try:
-                    for tyme in range(num_time):
-                # TODO: Needs to write commit on mongodb collection (still to define)
+                    for tyme in range(len(count_time)):
+                        # TODO: Needs to write commit on mongodb collection (still to define database model)
+                        print '*** Beginning ***'
+                        print datetime.datetime.now().isoformat()
+                        print 'Lat: ', lats[lat], ', Long: ', lons[lon], ', Time: ', tims[tyme]
+                        print str(vals[tyme, lats[lat], lons[lon]])
+                        print '*** End ***'
                 except Exception:
                     print 'Error while reading time'
+                    print Exception
         except Exception:
             print 'Error while reading longitudes'
-
+            print Exception
 except Exception:
     print 'Error while reading latitudes'
+    print Exception
