e529dc80a0494b4657674920f6fe6be3ef45d7a4
ricardobarroslourenco <ricardobarroslourenco@gmail.com>
2015-09-04 13:01:17 -0500

    Mongo Read class, initial commit.

---
+++ atlas/mongo_read.py
@@ -0,0 +1,8 @@
+__author__ = 'rblourenco@uchicago.edu'
+
+# Class for geospatial information retrieval on MongoDB
+# 2015-09-04 - Initial commit
+
+class mongo_read():
+    def __init__(self):
+        pass
