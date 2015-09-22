75c9e47e48f3ea1c246cf818b89aa5eab1ff8933
Nathan Matteson <matteson@obstructures.org>
2015-09-22 10:58:23 -0500

    New object for mongo ingestion

--- atlas/utils/nc4_to_mongodb.py
+++ atlas/utils/nc4_to_mongodb.py
@@ -1,43 +1,133 @@
-__author__ = "rblourenco@uchicago.edu"
-# 2015-08-19 - Initial commit
+from __future__ import division
 import os
 import sys
 import datetime
-import json
+import itertools
+try:
+    import simplejson as json
+except ImportError:
+    import json
 from pymongo.errors import PyMongoError
 from pymongo import MongoClient
 from netCDF4 import Dataset
 import geojson
-from atlas.constants import BASE_DIR
+from atlas.constants import BASE_DIR, MONGO
 
-# In case of being a local client
-# client = MongoClient('localhost', 27017)
 
-uri = "mongodb://user:password@example.com/the_database?authMechanism=SCRAM-SHA-1"
-client = MongoClient(uri)
+__author__ = "rblourenco@uchicago.edu"
+# 2015-08-19 - Initial commit
 
-start_time = datetime.datetime.now()
-print(' *** Start ***')
-print(str(start_time))
 
-# input location of the netCDF file
-nc_file = os.path.join(
-    BASE_DIR, 'data', 'netcdf', 'full_global',
-    'papsim_wfdei.cru_hist_default_firr_aet_whe_annual_1979_2012.nc4')
-
-# open the netCDF file
-nc_dataset = Dataset(nc_file, 'r')
-
-# netCDF variables
-longitude = 'lon'
-latitude = 'lat'
-time = 'time'
-sim_context = 'aet_whe'
-pixel_side_length = 0.001  # Using degree decimals - if zero, states a point
-
-# Defining MongoDB instance
+uri = "mongodb://{}:{}@{}/{}?authMechanism=SCRAM-SHA-1".format(
+    MONGO['user'], MONGO['password'], MONGO['domain'], MONGO['database']
+)
+client = MongoClient(uri) if not MONGO['local'] \
+    else MongoClient('localhost', MONGO['port'])
 db = client['atlas']
 points = db.simulation
+
+
+class NetCDFToMongo(object):
+    def __init__(self, file):
+        self.file = file
+        self.nc_dataset = Dataset(self.file, 'r')
+        # netCDF variables
+        self._lon_var = None
+        self._lat_var = None
+        self._time_var = None
+        self._sim_context = None
+        # Using degree decimals - if zero, states a point
+        self.pixel_side_length = 0.001
+
+    @property
+    def lat_var(self):
+        if self._lat_var is None:
+            self._lat_var = 'lat'
+        return self._lat_var
+
+    @property
+    def lon_var(self):
+        if self._lon_var is None:
+            self._lon_var = 'lon'
+        return self._lon_var
+
+    @property
+    def time_var(self):
+        if self._time_var is None:
+            self._time_var = 'time'
+        return self._time_var
+
+    @property
+    def sim_context(self):
+        if self._sim_context is None:
+            self._sim_context = 'aet_whe'
+        return self._sim_context
+
+    @property
+    def lats(self):
+        return self.nc_dataset.variables[self.lat_var][:]
+
+    @property
+    def lons(self):
+        return self.nc_dataset.variables[self.lon_var][:]
+
+    @property
+    def vals(self):
+        return self.nc_dataset.variables[self.sim_context][:, :, :]
+
+    @property
+    def tims(self):
+        return self.nc_dataset.variables[self.time_var][:]
+
+    @property
+    def count_lat(self):
+        return [int(i) for i in self.lats]
+
+    @property
+    def count_lon(self):
+        return [int(i) for i in self.lons]
+
+    @property
+    def count_time(self):
+        return [int(i) for i in self.tims]
+
+    def ingest(self):
+        start_time = datetime.datetime.now()
+        print('*** Start Run ***')
+        print(str(start_time))
+
+        try:
+            for lat, lon in itertools.product(self.lats, self.lons):
+                new_points = list()
+                try:
+                    for i, t in enumerate(self.tims):
+                        xx = str(self.vals[i, lat, lon])
+                        tile = geojson.dumps((
+                            GenerateDocument(lon, lat, self.sim_context, i, xx,
+                                             self.pixel_side_length)
+                        ), sort_keys=True)
+                        new_points.append(tile)
+                        tile = {}
+                except:
+                    print('Unexpected error:', sys.exc_info()[0])
+                    raise
+                new_points = [json.loads(coords) for coords in new_points]
+                result = points.insert_many(new_points)
+                # print '*** Inserted Points ***'
+                # print result.inserted_ids
+                # print '*** End Points ***'
+                new_points = []
+        except PyMongoError:
+            print('Error while committing on MongoDB')
+            raise
+        except:
+            print('Unexpected error:', sys.exc_info()[0])
+            raise
+
+        end_time = datetime.datetime.now()
+        elapsed_time = end_time - start_time
+        print('*** End Run ***')
+        print('Elapsed time:', str(elapsed_time))
 
 
 # Define GeoJSON standard for ATLAS
@@ -50,10 +140,17 @@ class GenerateDocument(object):
         self.valor = valor
         self.side = side
 
-    @property  # Attention: When referring to MongoDB User Reference, GeoJSON Standard 'geometry' should be used instead
-    # of 'loc', for geoindexing
+    @property
     def __geo_interface__(self):
-        # Define polygon based on centroid x,y and side
+        """Define polygon based on centroid (x, y) and side
+
+         ATTENTION: When referring to MongoDB user reference,
+         GeoJSON standard 'geometry' should be used instead of 'loc',
+         for geoindexing.
+
+        :return: GeoJSON object representing data point
+        :rtype: dict
+        """
         point_ax = self.x - (self.side / 2)
         point_ay = self.y + (self.side / 2)
         point_bx = self.x + (self.side / 2)
@@ -63,73 +160,25 @@ class GenerateDocument(object):
         point_dx = self.x - (self.side / 2)
         point_dy = self.y - (self.side / 2)
 
-        varOutput = {'type': 'Feature', 'centroid_x': self.x, 'centroid_y': self.y, 'geometry': {
-            'type': 'Polygon',
-            'coordinates': [
-                [[point_ax, point_ay], [point_bx, point_by], [point_cx, point_cy], [point_dx, point_dy],
-                 [point_ax, point_ay]]
-            ]
-        }, 'properties': {
-            'simulation': self.sim,
-            'timestamp': datetime.datetime.now().isoformat(),
-            'time': self.time,
-            'value': self.valor
-        }}
+        varOutput = {
+            'type': 'Feature', 'centroid_x': self.x, 'centroid_y': self.y,
+            'geometry': {'type': 'Polygon', 'coordinates': [
+                [[point_ax, point_ay], [point_bx, point_by],
+                 [point_cx, point_cy], [point_dx, point_dy],
+                 [point_ax, point_ay]]]},
+            'properties': {
+                'simulation': self.sim,
+                'timestamp': datetime.datetime.now().isoformat(),
+                'time': self.time,
+                'value': self.valor,
+            }}
+
         return varOutput
 
 
-# get netCDF variable objects
-latitudes = nc_dataset.variables[latitude]
-longitudes = nc_dataset.variables[longitude]
-values = nc_dataset.variables[sim_context]
-times = nc_dataset.variables[time]
-
-# get netCDF variable values
-lats = latitudes[:]
-lons = longitudes[:]
-vals = values[:, :, :]  # Sequence: Time, Latitude, Long
-tims = times[:]
-
-# make a list of latitudes and longitudes
-count_lat = [int(i) for i in lats]
-count_long = [int(i) for i in lons]
-count_time = [int(i) for i in tims]
-
-
-# populating the database
-try:
-    for lat in xrange(len(count_lat)):
-        try:
-            for lon in xrange(len(count_long)):
-                new_points = []
-                try:
-                    for tyme in xrange(len(count_time)):  # Loop in time: Fills time values on the GeoJSON
-                        xx = str(vals[tyme, lats[lat], lons[lon]])
-                        tile = geojson.dumps(
-                            (GenerateDocument(lons[lon], lats[lat], sim_context, tyme, xx, pixel_side_length)),
-                            sort_keys=True)
-                        new_points.append(tile)
-                        tile = {}  # Clear buffer                        
-                except:
-                    print('Unexpected error:', sys.exc_info()[0])
-                    raise
-                # commit new_points
-                new_points = [json.loads(coords) for coords in new_points]
-                result = points.insert_many(new_points)
-                # print '*** Inserted Points ***'
-                #print result.inserted_ids  # Give output of inserted values for a point on all times
-                # print '*** End ***'
-                new_points = []  # Clear Buffer
-        except PyMongoError:
-            print('Error while commiting on MongoDB')
-        except:
-            print('Unexpected error:', sys.exc_info()[0])
-            raise
-except:
-    print('Unexpected error:', sys.exc_info()[0])
-    raise
-end_time = datetime.datetime.now()
-elapsed_time = end_time - start_time
-print('**** End Run ********')
-print('Elapsed time')
-print(str(elapsed_time))
+if __name__ == '__main__':
+    nc_file = os.path.join(
+        BASE_DIR, 'data', 'netcdf', 'full_global',
+        'papsim_wfdei.cru_hist_default_firr_aet_whe_annual_1979_2012.nc4')
+    mi = NetCDFToMongo(nc_file)
+    mi.ingest()
