3e1060984fc077f0a2d8bd7f775117c1f63dea2d
Nathan Matteson <matteson@obstructures.org>
2015-12-01 07:24:01 -0600

    Pannable; loading icon

--- atlas_web/static/js/grid.js
+++ atlas_web/static/js/grid.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var world, data, grid_regions, top_left, bottom_right
+  var world, data, grid_regions, top_left, bottom_right, dims
     , height = window.innerHeight
     , width = window.innerWidth
     , current_year = 1979
@@ -15,6 +15,7 @@
       .attr({'width': width, 'height': height})
       .append('g')
     , ocean_layer = svg.append('g')
+      .attr('id', 'ocean_layer')
     , grid_layer = svg.append('g')
       .attr('id', 'grid_layer')
 
@@ -23,8 +24,8 @@
               '#f16913', '#d94801', '#a63603', '#7f2704'])
 
     , projection = d3.geo.equirectangular()
-      //.rotate([Options.lon, 0])
-      .center([Options.lon, Options.lat])
+      .rotate([-Options.lon, 0])
+      .center([0, Options.lat])
       .scale(height * 2)
       .translate([width / 2, height / 2])
       .precision(.1)
@@ -36,23 +37,37 @@
     ;
 
   var dragged = function dragged() {
-    var λ = d3.event.x * sens
-      , φ = d3.event.y * sens
+
+    var λ = d3.event.dx * sens
+      , φ = d3.event.dy * sens
       , upper_drag_limit = projection([0, 90])[1]
       , lower_drag_limit = projection([0, -90])[1] - height
     ;
 
-    φ = φ > lower_drag_limit ? lower_drag_limit
-      : φ < upper_drag_limit ? upper_drag_limit
+    φ = -φ > lower_drag_limit ? -lower_drag_limit
+      : -φ < upper_drag_limit ? -upper_drag_limit
       : φ;
-    projection.center(projection.invert([width / 2 - λ, height / 2 - φ]));
-    //path.projection(projection);
+
+    var c = projection.invert([
+      width / 2 - λ,
+      height / 2 - φ]);
+
+    projection
+      .rotate([-c[0], 0])
+      .center([0, c[1]]);
     svg.selectAll('.boundary').attr('d', path);
   };
 
+  var dragend = function dragend() {
+    get_data_for_viewport();
+  };
+
   var drag_rotate = d3.behavior.drag()
-      .origin(function() { var r = projection.rotate(); return {x: r[0] / sens, y: -r[1] / sens}; })
+      .origin(function() {
+        var r = projection.rotate();
+        return {x: r[0] / sens, y: -r[1] / sens}; })
       .on('drag', dragged)
+      .on('dragend', dragend)
     , scroll_zoom = d3.behavior.zoom()
       .translate(projection.translate())
       .scale(projection.scale())
@@ -82,6 +97,10 @@
   var get_viewport_dimensions = function get_viewport_dimensions() {
     top_left = projection.invert([0,0]);
     bottom_right = projection.invert([width, height]);
+
+    top_left[0] = bottom_right[0] > 180 ? top_left[0] - 180 : top_left[0];
+    bottom_right[0] = bottom_right[0] > 180 ? bottom_right[0] - 180 : bottom_right[0];
+
     top_right = [bottom_right[0], top_left[1]];
     bottom_left = [top_left[0], bottom_right[1]];
     center = [
@@ -90,6 +109,7 @@
         / 2 + top_left[0]),
       (top_left[1] - bottom_right[1]) / 2 + bottom_right[1]
     ];
+
     return {
       'top_left': top_left,
       'top_right': top_right,
@@ -104,9 +124,62 @@
     height = window.innerHeight;
     d3.select('svg').attr({height: height, width: width});
     projection.translate([width / 2, height / 2])
-      .center(dims.center)
       .scale(height * 2);
     d3.selectAll('.boundary').attr('d', path);
+  };
+
+  var show_loader = function show_loader() {
+    var loader = d3.select('#loader');
+    loader.style({
+      display: 'block',
+      top: (height - loader.node().getBoundingClientRect().height) / 2 + 'px',
+      left: (width - loader.node().getBoundingClientRect().width) / 2 + 'px'
+    });
+  };
+
+  var hide_loader = function show_loader() {
+    var loader = d3.select('#loader');
+    loader.style({ display: 'none' });
+  };
+
+  var draw_map_basics = function draw_map_basics() {
+
+    d3.json('/static/json/countries_110.geojson', function(world) {
+
+      var sphere = [
+        ocean_layer.append('path')
+          .datum({type: 'Sphere'})
+          .attr('id', 'sphere')
+          .attr('d', path)
+          .attr('class', 'boundary'),
+        ocean_layer.append('use')
+          .attr('class', 'stroke')
+          .attr('xlink:href', '#sphere')
+      ];
+
+      ocean_layer.append('path')
+        .datum(graticule)
+        .attr('class', 'graticule boundary')
+        .attr('d', path)
+        .style({
+          stroke: '#B4D5E5',
+          'stroke-width': '1px',
+          fill: 'transparent'
+        });
+
+      ocean_layer.selectAll('path.countries')
+        .data(world.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('class', 'countries boundary')
+        .style({
+          stroke: 'none',
+          fill: '#dddddd'
+        });
+
+    });
+
   };
 
   var update_data_fills = function update_data_fills() {
@@ -121,27 +194,37 @@
   };
 
   var grid_hover = function grid_hover(d) {
-    var q = d.geometry.coordinates[0] + ', ';
-    q += d.geometry.coordinates[1] + ': ';
-    q += d.properties.value;
-    hover_legend.select('p').text(q);
+    var q = '[' + (d.geometry.coordinates[0][0][0] +.25) + ', ';
+    q += (d.geometry.coordinates[0][0][1] +.25) + '] <b>';
+    q += (Math.round(d.properties.value.values[_time] * 100) / 100) + '</b>' ;
+    hover_legend.select('p').html(q);
     hover_legend.style({
-      left: d3.event.mouse[0] + 'px',
-      top: d3.event.mouse[1] + 'px',
+      left: d3.mouse(this)[0] + 'px',
+      top: d3.mouse(this)[1] + 'px',
       display: 'block'
     });
+    hover_legend.classed('hovered', true);
+  };
+
+  var get_data_for_viewport = function get_data_for_viewport() {
+
+    show_loader();
+    dims = get_viewport_dimensions();
+
+    queue()
+      .defer(d3.json, '/api/'+
+        dims['top_left'][0]+'/'+ dims['top_left'][1]+'/'+
+        dims['bottom_right'][0]+'/'+dims['bottom_right'][1])
+      .awaitAll(atlas);
   };
 
   var atlas = function atlas(error, queued_data) {
 
     data = queued_data[0];
-    world = queued_data[1];
     data.filter(function (d) { return d.properties.value != null; });
     data.forEach(function(d) {
       d.geometry.coordinates.reverse();
     });
-
-    projection.center(dims.center);
 
     color.domain([
       d3.min(data, function(d) {
@@ -149,61 +232,27 @@
       d3.max(data, function(d) {
         return d3.max(d.properties.value.values, function(dd) {return dd; }); })]);
 
-    var sphere = [
-      ocean_layer.append('path')
-        .datum({type: 'Sphere'})
-        .attr('id', 'sphere')
-        .attr('d', path)
-        .attr('class', 'boundary'),
-      ocean_layer.append('use')
-        .attr('class', 'stroke')
-        .attr('xlink:href', '#sphere')
-      ];
-
-    ocean_layer.append('path')
-      .datum(graticule)
-      .attr('class', 'graticule boundary')
-      .attr('d', path)
-      .style({
-        stroke: '#B4D5E5',
-        'stroke-width': '1px',
-        fill: 'transparent'
-      });
-
-    ocean_layer.selectAll('path.countries')
-      .data(world.features)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      .attr('class', 'countries boundary')
-      .style({
-        stroke: 'none',
-        fill: '#dddddd'
-      });
-
-    grid_regions = grid_layer.selectAll('.grid-boundary')
-      .data(data)
-      .enter()
+     grid_regions = grid_layer.selectAll('.grid-boundary')
+      .data(data);
+    grid_regions.enter()
       .append('path')
       .attr('class', 'grid-boundary boundary')
       .attr('d', path)
       .on('mouseover', grid_hover)
-      .on('mouseout', function() { hover_legend.style({display: 'none'})})
+      .on('mouseout', function() {
+        hover_legend.style({display: 'none'});
+        hover_legend.classed('hovered', false);})
     ;
+
+    grid_regions.exit().remove();
 
     update_data_fills(data);
     svg.call(drag_rotate);
 
+    svg.selectAll('.boundary').attr('d', path);
+    hide_loader();
+
   };
-
-  var dims = get_viewport_dimensions();
-
-  queue()
-    .defer(d3.json, '/api/'+
-      dims['top_left'][0]+'/'+ dims['top_left'][1]+'/'+
-      dims['bottom_right'][0]+'/'+dims['bottom_right'][1])
-    .defer(d3.json, '/static/json/countries_110.geojson')
-    .awaitAll(atlas);
 
   var time_opt = d3.select('#time_select');
   var time_label = d3.select('#menu_time label');
@@ -215,7 +264,12 @@
     d3.select('#corner_legend [data-type="time"]').text(current_year);
     update_data_fills(data);
   });
-
   d3.select(window).on('resize', resize);
+
+  get_data_for_viewport();
+
+  draw_map_basics();
+
+
 
 })();
