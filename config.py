dbace1f22fb49f47457f7844ad1c1312399f80e3
njmattes <matteson@obstructures.org>
2016-05-24 09:06:04 -0500

    Remove unused variable in config

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-DEBUG_ASSETS = True
-# ASSETS_DEBUG = True
+ASSETS_DEBUG = True
 
 ADMINS = frozenset(['matteson@obstructures.org'])
 SECRET_KEY = 'REPLACEME'
