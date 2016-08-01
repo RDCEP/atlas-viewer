78cf89f7d46f505b253094240ad9e6de1e78e65d
njmattes <matteson@obstructures.org>
2016-08-01 08:23:41 -0500

    Viewbox etc.

--- atlas_web/static/js/app/atlas.map.v0.js
+++ atlas_web/static/js/app/atlas.map.v0.js
@@ -5,9 +5,11 @@ var get_map_scale = function get_map_scale() {
 
 };
 
-var svgroot = d3.select('#map').append('svg')
+var svgwrap = d3.select('#map')
+  , svgroot = svgwrap.append('svg')
     .attr('width', width)
     .attr('height', height)
+    .attr('viewBox', '0 0 ' + width + ' ' + height)
   , filter = svgroot.append('defs')
     .append('filter')
     .attr('id', 'grid_filter')
